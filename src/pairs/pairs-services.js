4a0c4f65ab051cc8b55fc0acfb547e701b131097
Hsin Ling Hu <hsinkava11@gmail.com>
2020-09-12 22:47:39 -0700

    revised pairs-services

--- src/pairs/pairs-services.js
+++ src/pairs/pairs-services.js
@@ -19,20 +19,24 @@ const PairsService={
         return rows[0];
       });
   }, 
-  getMatchListener(db, id){
+  getMatchSpeaker(db, m_topic, m_gender, m_age){
     return db
       .from('listeners')
       .select('listeners.*', 'mpath_users.user_age', 'mpath_users.user_gender', 'mpath_users.user_name')
       .rightJoin('mpath_users', 'mpath_users.id', 'listeners_user_id')
-      .where('listeners_user_id', id)
+      .where('topic', m_topic)
+      .where('spe_gender', m_gender)
+      .where('spe_age', m_age)
       .first();
   },
-  getMatchSpeaker(db, id){
+  getMatcListener(db, m_topic, m_gender, m_age){
     return db
       .from('speakers')
       .select('speakers.*', 'mpath_users.user_age', 'mpath_users.user_gender', 'mpath_users.user_name')
       .rightJoin('mpath_users', 'mpath_users.id', 'speakers_user_id')
-      .where('speakers_user_id', id)
+      .where('topic', m_topic)
+      .where('lis_gender', m_gender)
+      .where('lis_age', m_age)
       .first();
   }
-}
+};
